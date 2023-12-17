da045304308db184e664af9be33f0ac51e93f2e0
Zehan Khan <zkhan@hootboard.com>
2023-12-17 19:19:41 +0530

    fix: :bug: fix issue

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,18 +1,13 @@
 import { initializeApp } from "firebase/app";
 import {
   getAuth,
-  signInWithPopup,
   signInWithEmailAndPassword,
   createUserWithEmailAndPassword,
-  sendPasswordResetEmail,
   signOut,
 } from "firebase/auth";
 import {
   getFirestore,
-  query,
-  getDocs,
   collection,
-  where,
   addDoc,
 } from "firebase/firestore";
 
